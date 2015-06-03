0ed1e13115a9524875105322f16998e4de2c4efa
James Lu <GLolol1@hotmail.com>
2015-06-03 16:09:53 -0700

    Remove plugins/hello.py
    This was solely a testing module for the plugin/commands system, and serves no purpose anymore. commands.py provides basic commands that are both simple and actually useful.

--- plugins/hello.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import utils
-
-@utils.add_cmd
-def hello(irc, source, args):
-   utils.msg(irc, source, 'hello!')
