61804b1ecdec97e8c8a46b12e501fc1b8208263e
James Lu <GLolol1@hotmail.com>
2015-07-18 20:11:29 -0700

    log: also log to $curdir/log/pylink.log
    Closes #52.

--- log.py
+++ log.py
@@ -1,15 +1,29 @@
 import logging
 import sys
+import os
+
 from conf import conf
 
-level = conf['bot']['loglevel'].upper()
+level = conf['bot'].get('loglevel') or 'DEBUG'
 try:
-    level = getattr(logging, level)
+    level = getattr(logging, level.upper())
 except AttributeError:
     print('ERROR: Invalid log level %r specified in config.' % level)
     sys.exit(3)
 
-logging.basicConfig(level=level, format='%(asctime)s [%(levelname)s] %(message)s')
+curdir = os.path.dirname(os.path.realpath(__file__))
+logdir = os.path.join(curdir, 'log')
+# Make sure our log/ directory exists
+os.makedirs(logdir, exist_ok=True)
+
+_format = '%(asctime)s [%(levelname)s] %(message)s'
+logging.basicConfig(level=level, format=_format)
+
+# Set log file to $CURDIR/log/pylink
+logformat = logging.Formatter(_format)
+logfile = logging.FileHandler(os.path.join(logdir, 'pylink.log'))
+logfile.setFormatter(logformat)
 
 global log
 log = logging.getLogger()
+log.addHandler(logfile)
