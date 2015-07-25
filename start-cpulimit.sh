5ab7b507be89466b6eb0e1514bb2ff78a27222c9
James Lu <GLolol1@hotmail.com>
2015-07-25 16:58:11 -0700

    Add wrapper scripts (start-cpulimit.sh, kill.sh) to assist running PyLink under cpulimit
    Again, we're trying to prevent the software from frying people's CPUs...

---
+++ start-cpulimit.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Shell script to start PyLink under CPUlimit, killing it if it starts abusing the CPU.
+
+# Set this to whatever you want. cpulimit --help
+LIMIT=50
+
+# Change to the PyLink root directory.
+WRAPPER_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd "$WRAPPER_DIR"
+
+if [[ ! -z "$(which cpulimit)" ]]; then
+	# -k kills the PyLink daemon if it goes over $LIMIT
+	# -z makes cpulimit exit when PyLink dies.
+	cpulimit -l $LIMIT -z -k ./main.py
+	echo "PyLink has been started (daemonized) under cpulimit, and will automatically be killed if it goes over the CPU limit of ${LIMIT}%."
+	echo "To kill the process manually, run ./kill.sh"
+else
+	echo 'cpulimit not found in $PATH! Aborting.'
+fi
