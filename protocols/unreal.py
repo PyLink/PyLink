9cd1635f68dafee47f147de43b258014d14da6e2
James Lu <GLolol@overdrivenetworks.com>
2016-03-26 12:50:23 -0700

    unreal: fix wrong variable name in handle_umode2

--- protocols/unreal.py
+++ protocols/unreal.py
@@ -673,7 +673,7 @@ class UnrealProtocol(TS6BaseProtocol):
 
         if ('+o', None) in parsedmodes:
             # If +o being set, call the CLIENT_OPERED internal hook.
-            self.irc.callHooks([uid, 'CLIENT_OPERED', {'text': 'IRC_Operator'}])
+            self.irc.callHooks([numeric, 'CLIENT_OPERED', {'text': 'IRC_Operator'}])
 
         self.checkCloakChange(numeric, parsedmodes)
 
