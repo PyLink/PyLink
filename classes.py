PyLink/PyLink

commit 0c7faed213eb42d15fbbccb9a2bc24014d7584e7

Move NotAuthenticatedError class back into utils
This is needed because ServiceBot exists in utils, and utils cannot import classes without a bad import loop happening.

classes.py
@@ -27,13 +27,6 @@ import structures
 class ProtocolError(Exception):
     pass
 
-class NotAuthenticatedError(Exception):
-    """
-    Exception raised by checkAuthenticated() when a user fails authentication
-    requirements.
-    """
-    pass
-
 ### Internal classes (users, servers, channels)
 
 class Irc():
@@ -898,7 +891,7 @@ class Irc():
         if not self.isOper(uid, allowAuthed=allowAuthed, allowOper=allowOper):
             log.warning('(%s) Access denied for %s calling %r', self.name,
                         self.getHostmask(uid), lastfunc)
-            raise NotAuthenticatedError("You are not authenticated!")
+            raise utils.NotAuthenticatedError("You are not authenticated!")
         return True
 
 class IrcUser():

utils.py
@@ -15,6 +15,13 @@ from log import log
 import world
 import conf
 
+class NotAuthenticatedError(Exception):
+    """
+    Exception raised by checkAuthenticated() when a user fails authentication
+    requirements.
+    """
+    pass
+
 class IncrementalUIDGenerator():
     """
     Incremental UID Generator module, adapted from InspIRCd source:

commit 9236f7e09521199744d9bd4b7e9a85860574bef7

Irc: fix NameError caused by 0c7faed

classes.py
@@ -470,7 +470,7 @@ class Irc():
         for func in world.commands[cmd]:
             try:
                 func(self, source, cmd_args)
-            except NotAuthenticatedError:
+            except utils.NotAuthenticatedError:
                 self.msg(self.called_by or source, 'Error: You are not authorized to perform this operation.')
             except Exception as e:
                 log.exception('Unhandled exception caught in command %r', cmd)