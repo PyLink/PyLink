2f188dc60d24d6a2df2d1c87ca017664913b30b1
James Lu <GLolol@overdrivenetworks.com>
2016-06-21 10:55:42 -0700

    core: Remove load-conf-on-import, implement basic command line options via argparse
    Ref #242.

--- conf.py
+++ conf.py
@@ -19,37 +19,6 @@ from collections import defaultdict
 
 from . import world
 
-global testconf
-testconf = {'bot':
-                {
-                    'nick': 'PyLink',
-                    'user': 'pylink',
-                    'realname': 'PyLink Service Client',
-                    'serverdesc': 'PyLink unit tests'
-                },
-            'logging':
-                {
-                    # Suppress logging in the test output for the most part.
-                    'stdout': 'CRITICAL'
-                },
-            'servers':
-                # Wildcard defaultdict! This means that
-                # any network name you try will work and return
-                # this basic template:
-                defaultdict(lambda: {
-                        'ip': '0.0.0.0',
-                        'port': 7000,
-                        'recvpass': "abcd",
-                        'sendpass': "chucknorris",
-                        'protocol': "null",
-                        'hostname': "pylink.unittest",
-                        'sid': "9PY",
-                        'channels': ["#pylink"],
-                        'maxnicklen': 20,
-                        'sidrange': '8##'
-                    })
-           }
-
 def validateConf(conf):
     """Validates a parsed configuration dict."""
     assert type(conf) == dict, "Invalid configuration given: should be type dict, not %s." % type(conf).__name__
@@ -72,9 +41,12 @@ def validateConf(conf):
 
 def loadConf(fname, errors_fatal=True):
     """Loads a PyLink configuration file from the filename given."""
+    global confname, conf
+    confname = fname.split('.', 1)[0]
     with open(fname, 'r') as f:
         try:
             conf = yaml.load(f)
+            conf = validateConf(conf)
         except Exception as e:
             print('ERROR: Failed to load config from %r: %s: %s' % (fname, type(e).__name__, e))
             if errors_fatal:
@@ -82,21 +54,3 @@ def loadConf(fname, errors_fatal=True):
             raise
         else:
             return conf
-
-if world.testing:
-    conf = testconf
-    confname = 'testconf'
-    fname = None
-else:
-    try:
-        # Get the config name from the command line, falling back to config.yml
-        # if not given.
-        fname = sys.argv[1]
-        confname = fname.split('.', 1)[0]
-    except IndexError:
-        # confname is used for logging and PID writing, so that each
-        # instance uses its own files. fname is the actual name of the file
-        # we load.
-        confname = 'pylink'
-        fname = 'config.yml'
-    conf = validateConf(loadConf(fname))
