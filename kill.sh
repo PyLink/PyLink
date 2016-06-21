1caf42e8c378d55a7ad778b03f1ba751911e8256
James Lu <GLolol@overdrivenetworks.com>
2016-06-20 17:11:27 -0700

    Remove CPUlimit wrapper scripts

--- kill.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Script to kill PyLink quickly when running under CPUlimit, since
-# it will daemonize after threads are spawned and Ctrl-C won't work.
-
-kill $(cat pylink.pid)
-echo 'Killed. Press Ctrl-C in the PyLink window to exit.'
