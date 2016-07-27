PyLink/PyLink

commit fca23c7d5594ba098324c0236bd5f6f62488e93b

nefarious: forward CMODE and KICK through the server if the sender isn't opped
This prevents mode bounces, kick failures, and the HACK server notices from showing up.

protocols/nefarious.py
@@ -352,6 +352,13 @@ class P10Protocol(IRCS2SProtocol):
         if not reason:
             reason = 'No reason given'
 
+        cobj = self.irc.channels[channel]
+        # HACK: prevent kick bounces by sending our kick through the server if
+        # the sender isn't op.
+        if numeric not in self.irc.servers and (not cobj.isOp(numeric)) and (not cobj.isHalfop(numeric)):
+            reason = '(%s) %s' % (self.irc.getFriendlyName(numeric), reason)
+            numeric = self.irc.getServer(numeric)
+
         self._send(numeric, 'K %s %s :%s' % (channel, target, reason))
 
         # We can pretend the target left by its own will; all we really care about
@@ -389,7 +396,6 @@ class P10Protocol(IRCS2SProtocol):
                 (not self.irc.isInternalServer(numeric)):
             raise LookupError('No such PyLink client/server exists.')
 
-        self.irc.applyModes(target, modes)
         modes = list(modes)
 
         # According to the P10 specification:
@@ -401,11 +407,19 @@ class P10Protocol(IRCS2SProtocol):
             cobj = self.irc.channels[self.irc.toLower(target)]
             ts = ts or cobj.ts
             send_ts = True
+
+            # HACK: prevent mode bounces by sending our mode through the server if
+            # the sender isn't op.
+            if numeric not in self.irc.servers and (not cobj.isOp(numeric)) and (not cobj.isHalfop(numeric)):
+                numeric = self.irc.getServer(numeric)
+
         else:
             assert target in self.irc.users, "Unknown mode target %s" % target
             # P10 uses nicks in user MODE targets, NOT UIDs. ~GL
             target = self.irc.users[target].nick
             send_ts = False
+
+        self.irc.applyModes(target, modes)
 
         while modes[:12]:
             joinedmodes = self.irc.joinModes([m for m in modes[:12]])

commit af210638342651dcb86c83319231dbb5ed4de98d

nefarious: fix usermode changes internally applying on the wrong target
A regression from fca23c7d5594ba098324c0236bd5f6f62488e93b overwrote the target UID with a nick before passing it into applyModes(), which raises an "unknown mode target" warning and causes the mode change to be dropped.

protocols/nefarious.py
@@ -413,10 +413,11 @@ class P10Protocol(IRCS2SProtocol):
             if numeric not in self.irc.servers and (not cobj.isOp(numeric)) and (not cobj.isHalfop(numeric)):
                 numeric = self.irc.getServer(numeric)
 
+            real_target = target
         else:
             assert target in self.irc.users, "Unknown mode target %s" % target
             # P10 uses nicks in user MODE targets, NOT UIDs. ~GL
-            target = self.irc.users[target].nick
+            real_target = self.irc.users[target].nick
             send_ts = False
 
         self.irc.applyModes(target, modes)
@@ -424,7 +425,7 @@ class P10Protocol(IRCS2SProtocol):
         while modes[:12]:
             joinedmodes = self.irc.joinModes([m for m in modes[:12]])
             modes = modes[12:]
-            self._send(numeric, 'M %s %s%s' % (target, joinedmodes, ' %s' % ts if send_ts else ''))
+            self._send(numeric, 'M %s %s%s' % (real_target, joinedmodes, ' %s' % ts if send_ts else ''))
 
     def nick(self, numeric, newnick):
         """Changes the nick of a PyLink client."""