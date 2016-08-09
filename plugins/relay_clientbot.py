c09ff6a7065e65d0095b18267b5e7844c5107249
James Lu <GLolol@overdrivenetworks.com>
2016-08-08 22:23:42 -0700

    relay_clientbot: fix config key name

--- plugins/relay_clientbot.py
+++ plugins/relay_clientbot.py
@@ -47,7 +47,7 @@ def cb_relay_core(irc, source, command, args):
         relay_conf = conf.conf.get('relay', {})
 
         # Be less floody on startup: don't relay non-PRIVMSGs for the first X seconds after connect.
-        startup_delay = relay_conf.get('startup_delay', 5)
+        startup_delay = relay_conf.get('clientbot_startup_delay', 5)
 
         # Special case for CTCPs.
         if real_command == 'MESSAGE':
