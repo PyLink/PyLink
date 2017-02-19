03fc16dd5ac6360cbd38d0284b9353bf27b4edd0
James Lu <GLolol@overdrivenetworks.com>
2017-02-18 19:47:36 -0800

    Irc: rewrite parseArgs to be more efficient

--- classes.py
+++ classes.py
@@ -1327,27 +1327,21 @@ class Protocol():
         port = self.irc.serverdata['port']
         assert type(port) == int and 0 < port < 65535, "Invalid port %r for network %s" % (port, self.irc.name)
 
-    def parseArgs(self, args):
+    @staticmethod
+    def parseArgs(args):
         """Parses a string of RFC1459-style arguments split into a list, where ":" may
         be used for multi-word arguments that last until the end of a line.
         """
         real_args = []
         for idx, arg in enumerate(args):
-            real_args.append(arg)
-            # If the argument starts with ':' and ISN'T the first argument.
-            # The first argument is used for denoting the source UID/SID.
             if arg.startswith(':') and idx != 0:
-                # : is used for multi-word arguments that last until the end
-                # of the message. We can use list splicing here to turn them all
-                # into one argument.
-                # Set the last arg to a joined version of the remaining args
-                arg = args[idx:]
-                arg = ' '.join(arg)[1:]
-                # Cut the original argument list right before the multi-word arg,
-                # and then append the multi-word arg.
-                real_args = args[:idx]
-                real_args.append(arg)
+                # ":" is used to begin multi-word arguments that last until the end of the message.
+                # Use list splicing here to join them into one argument, and then add it to our list of args.
+                joined_arg = ' '.join(args[idx:])[1:]  # Cut off the leading : as well
+                real_args.append(joined_arg)
                 break
+            real_args.append(arg)
+
         return real_args
 
     def removeClient(self, numeric):
