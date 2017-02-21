f83a81242a4483182a8b0fdbfd4fe77c5ee06768
James Lu <GLolol@overdrivenetworks.com>
2017-02-20 19:31:31 -0800

    conf: reuse already-fetched newlogins value

--- conf.py
+++ conf.py
@@ -73,7 +73,7 @@ def validateConf(conf, logger=None):
 
     assert conf['login'].get('password') != "changeme", "You have not set the login details correctly!"
 
-    if conf['login'].get('accounts'):
+    if newlogins:
         assert conf.get('permissions'), "New-style accounts enabled but no permissions block was found. You will not be able to administrate your PyLink instance!"
 
     return conf
