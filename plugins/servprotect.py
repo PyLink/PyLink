1938a0097222ee3f972c4b8388af824f9a8c4c4e
Ken Spencer <ken@electrocode.net>
2017-02-21 18:06:25 -0500

    servprotect: try .get() on each so we can default out

--- plugins/servprotect.py
+++ plugins/servprotect.py
@@ -8,8 +8,8 @@ from pylinkirc.log import log
 # we've already checked this combinations sanity.
 # so lets set this up
 
-length = conf['servprotect'].get('length', 5)
-age    = conf['servprotect'].get('max_age', 10)
+length = conf.get('servprotect').get('length', 5)
+age    = conf.get('servprotect').get('max_age', 10)
 
 savecache = ExpiringDict(max_len=length, max_age_seconds=age)
 killcache = ExpiringDict(max_len=length, max_age_seconds=age)
