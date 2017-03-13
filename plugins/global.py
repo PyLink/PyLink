a3dff204d31d916b78d63acdba6301e9805daf84
James Lu <GLolol@overdrivenetworks.com>
2017-03-13 13:21:11 -0700

    global: remove extraneous metadata
    Core plugins do not need to track authors and version, because git does that for us.

--- plugins/global.py
+++ plugins/global.py
@@ -1,8 +1,5 @@
 # global.py: Global Noticing Plugin
 
-__authors__ = [("Ken Spencer", "Iota <ken@electrocode.net>")]
-__version__ = "0.0.1"
-
 from pylinkirc import conf, utils, world
 from pylinkirc.log import log
 from pylinkirc.coremods import permissions
