3ffbbe60ff67078d679082f804271deaf728e0d0
James Lu <GLolol@overdrivenetworks.com>
2017-03-13 13:38:53 -0700

    global: configurable output format

--- plugins/global.py
+++ plugins/global.py
@@ -1,8 +1,12 @@
 # global.py: Global Noticing Plugin
 
+import string
+
 from pylinkirc import conf, utils, world
 from pylinkirc.log import log
 from pylinkirc.coremods import permissions
+
+DEFAULT_FORMAT = "[$sender@$fullnetwork] $text"
 
 def g(irc, source, args):
     """<message text>
@@ -11,12 +15,21 @@ def g(irc, source, args):
     """
     permissions.checkPermissions(irc, source, ["global.global"])
     message = " ".join(args)
-    message = message + " (sent by %s@%s)" % (irc.getFriendlyName(irc.called_by), irc.getFullNetworkName())
+    template = string.Template(conf.conf.get('global', {}).get("format", DEFAULT_FORMAT))
+
     for name, ircd in world.networkobjects.items():
         if ircd.connected.is_set():  # Only attempt to send to connected networks
             for channel in ircd.pseudoclient.channels:
+                subst = {'sender': irc.getFriendlyName(source),
+                         'network': irc.name,
+                         'fullnetwork': irc.getFullNetworkName(),
+                         'current_channel': channel,
+                         'current_network': ircd.name,
+                         'current_fullnetwork': ircd.getFullNetworkName(),
+                         'text': message}
+
                 # Disable relaying or other plugins handling the global message.
-                ircd.msg(channel, message, loopback=False)
+                ircd.msg(channel, template.safe_substitute(subst), loopback=False)
 
 
 utils.add_cmd(g, "global", featured=True)
