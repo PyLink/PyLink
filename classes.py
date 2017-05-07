de67fe0d377a5e2a449598e8004e514d53b5a9fb
James Lu <GLolol@overdrivenetworks.com>
2017-05-07 13:46:15 -0700

    Irc: log socket shutdown errors to debug

--- classes.py
+++ classes.py
@@ -386,8 +386,8 @@ class Irc(utils.DeprecatedAttributesObject):
         try:
             log.debug('(%s) disconnect: Shutting down socket.', self.name)
             self.socket.shutdown(socket.SHUT_WR)
-        except:  # Socket timed out during creation; ignore
-            pass
+        except Exception as e:  # Socket timed out during creation; ignore
+            log.debug('(%s) error on socket shutdown: %s: %s', self.name, type(e).__name__, e)
 
         self.socket.close()
 
