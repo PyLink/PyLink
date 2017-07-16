1f377adfeeadb1279210272b263f7f87b98eba2d
James Lu <james@overdrivenetworks.com>
2017-07-16 07:34:45 -0700

    p10: also acknowledge our own kicks with a PART
    (adapted from commit 1438f9e566a8f1778a448fd3fb3d1fc9da90a26b)

--- protocols/p10.py
+++ protocols/p10.py
@@ -365,6 +365,11 @@ class P10Protocol(IRCS2SProtocol):
         # handle_part() does that just fine.
         self.handle_part(target, 'KICK', [channel])
 
+        if self.irc.isInternalClient(target):
+            # Send PART in response to acknowledge the KICK, per
+            # https://github.com/evilnet/nefarious2/blob/ed12d64/doc/p10.txt#L611-L616
+            self._send(target, 'L %s :%s' % (channel, reason))
+
     def kill(self, numeric, target, reason):
         """Sends a kill from a PyLink client/server."""
         # <- ABAAA D AyAAA :nefarious.midnight.vpn!GL (test)
