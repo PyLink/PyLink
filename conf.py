PyLink/PyLink

commit 2b346e3c01c094922e3d96411370c8394f5e38d4

conf: use Python logging when avaiable when the config file fails to load

conf.py
@@ -121,10 +121,15 @@ def loadConf(filename, errors_fatal=True, logger=None):
             conf = yaml.load(f)
             conf = validateConf(conf, logger=logger)
     except Exception as e:
-        print('ERROR: Failed to load config from %r: %s: %s' % (filename, type(e).__name__, e), file=sys.stderr)
+        e = 'Failed to load config from %r: %s: %s' % (filename, type(e).__name__, e)
 
         if errors_fatal:
             sys.exit(1)
+        elif logger:  # Prefer using the Python logger when available
+            logger.exception(e)
+        else:  # Otherwise, fall back to a print() call.
+            print('ERROR: %s' % e, file=sys.stderr)
+
         raise
     else:
         return conf

commit 7e8ff516461bad6f5793fedb312982c473c8b5fb

conf: when config loading fails, show an error /before/ quitting
This fixes a regression from 2b346e3c01c094922e3d96411370c8394f5e38d4.

conf.py
@@ -123,12 +123,13 @@ def loadConf(filename, errors_fatal=True, logger=None):
     except Exception as e:
         e = 'Failed to load config from %r: %s: %s' % (filename, type(e).__name__, e)
 
-        if errors_fatal:
-            sys.exit(1)
-        elif logger:  # Prefer using the Python logger when available
+        if logger:  # Prefer using the Python logger when available
             logger.exception(e)
         else:  # Otherwise, fall back to a print() call.
             print('ERROR: %s' % e, file=sys.stderr)
+
+        if errors_fatal:
+            sys.exit(1)
 
         raise
     else: