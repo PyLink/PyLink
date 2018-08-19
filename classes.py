49badd1665983450497c218678547ea97e364e4c
James Lu <james@overdrivenetworks.com>
2018-08-19 19:41:29 -0400

    updateTS: silently ignore messages with ts = 0
    Closes #625.

--- classes.py
+++ classes.py
@@ -1473,7 +1473,8 @@ class PyLinkNetworkCoreWithUtils(PyLinkNetworkCore):
 
             elif (their_ts < our_ts):
                 if their_ts < 750000:
-                    log.warning('(%s) Possible desync? Not setting bogus TS %s on channel %s', self.name, their_ts, channel)
+                    if their_ts != 0:  # Sometimes unreal sends SJOIN with 0, don't warn for those
+                        log.warning('(%s) Possible desync? Not setting bogus TS %s on channel %s', self.name, their_ts, channel)
                 else:
                     log.debug('(%s) Resetting channel TS of %s from %s to %s (remote has lower TS)',
                               self.name, channel, our_ts, their_ts)
