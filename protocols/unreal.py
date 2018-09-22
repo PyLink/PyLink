PyLink/PyLink

commit fac6fe506f9c58c3d96b0f6bea67a04da4a64632

unreal: use SJOIN in join() to work around weird TS corrupting bugs
This seems to be what Unreal itself does anyways.

protocols/unreal.py
@@ -106,9 +106,14 @@ class UnrealProtocol(TS6BaseProtocol):
         """Joins a PyLink client to a channel."""
         if not self.is_internal_client(client):
             raise LookupError('No such PyLink client exists.')
-        self._send_with_prefix(client, "JOIN %s" % channel)
-        self._channels[channel].users.add(client)
-        self.users[client].channels.add(channel)
+
+        # Forward this on to SJOIN, as using JOIN in Unreal S2S seems to cause TS corruption bugs.
+        # This seems to be what Unreal itself does anyways.
+        if channel not in self.channels and prefix == '':
+            prefix = 'o'  # Create new channels with the first joiner as op
+        else:
+            prefix = ''
+        self.sjoin(self.sid, channel, [(prefix, client)])
 
     def sjoin(self, server, channel, users, ts=None, modes=set()):
         """Sends an SJOIN for a group of users to a channel.

commit 2f4476eb0cb91fbcb3ae18ec0655351c12643773

unreal: remove invalid comparison
(Regression from commit fac6fe506f9c58c3d96b0f6bea67a04da4a64632)

protocols/unreal.py
@@ -112,7 +112,7 @@ class UnrealProtocol(TS6BaseProtocol):
 
         # Forward this on to SJOIN, as using JOIN in Unreal S2S seems to cause TS corruption bugs.
         # This seems to be what Unreal itself does anyways.
-        if channel not in self.channels and prefix == '':
+        if channel not in self.channels:
             prefix = 'o'  # Create new channels with the first joiner as op
         else:
             prefix = ''