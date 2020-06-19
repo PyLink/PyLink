7606680ffca84503f5276f8b46521839c2187b26
Celelibi <celelibi@gmail.com>
2020-06-19 03:50:17 +0200

    plugin stats: Refactor multiline function call
    Signed-off-by: Celelibi <celelibi@gmail.com>

--- plugins/stats.py
+++ plugins/stats.py
@@ -115,11 +115,9 @@ def handle_stats(irc, source, command, args):
         for accountname, accountdata in conf.conf['login'].get('accounts', {}).items():
             networks = accountdata.get('networks', [])
             if irc.name in networks or not networks:
-                _num(243, "O %s * %s :%s" % (
-                     ' '.join(accountdata.get('hosts', ['*@*'])),
-                     accountname,
-                     'needoper' if accountdata.get('require_oper') else '')
-                 )
+                hosts = ' '.join(accountdata.get('hosts', ['*@*']))
+                needoper = 'needoper' if accountdata.get('require_oper') else ''
+                _num(243, "O %s * %s :%s" % (hosts, accountname, needoper))
 
     elif stats_type == 'u':
         # 242/RPL_STATSUPTIME: ":Server Up <days> days <hours>:<minutes>:<seconds>"
